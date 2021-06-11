57ce410c64d0894b75d86052fcaa81efdde814d4
flaviomorelli <38486294+flaviomorelli@users.noreply.github.com>
2021-06-11 12:21:23 +0200

    Added multiple simulations in Julia

--- code/utils/simulations/sim_models.jl
+++ code/utils/simulations/sim_models.jl
@@ -2,9 +2,98 @@ using Distributions, Turing
 
 include("gb2.jl")
 
-initialize(n_var, dim) = [Vector{Float32}(undef, dim) for _ in 1:n_var]
+initialize(n_var, dim) = [Vector{Float64}(undef, dim) for _ in 1:n_var]
 
-@model function logscale(N, D, group)
+corr_matrix(dim, ρ) = I(dim) + abs.(I(dim) .- 1) * ρ
+
+@model function logscale(N, D, K, group; ρ=0.2)
+    y, ε = initialize(2, N)
+    u = Vector{Float64}(undef, D) 
+    X = Matrix{Float64}(undef, N, K)
+
+    μ = Matrix{Float64}(undef, D, K)
+    Σ = corr_matrix(K, ρ)    
+
+    β = repeat([0.1], K)
+    
+    for d in 1:D
+        u[d] ~ Normal(0, 0.4)
+        for k in 1:K 
+            μ[d, k] ~ Uniform(2, 3)
+        end
+    end
+    
+    for n in 1:N
+        ε[n] ~ Normal(0, 0.3)
+        X[n, :] ~ MvNormal(μ[group[n], :], Σ)
+        y[n] = exp(5 + X[n, :] ⋅ β  + u[group[n]] + ε[n])
+    end 
+
+    return (y = y, X = X, ε = ε, u = u)
+end
+
+@model function pareto(N, D, K, group; ρ=0.2)
+    y, ε = initialize(2, N)
+    u = Vector{Float64}(undef, D)
+    X = Matrix{Float64}(undef, N, K)
+
+    μ = Matrix{Float64}(undef, D, K)
+    Σ = corr_matrix(K, ρ)    
+
+    β = repeat([-350], K)
+    
+    for d in 1:D
+        u[d] ~ Normal(0, 500)
+        for k in 1:K 
+            μ[d, k] ~ Uniform(-3, 3)
+        end
+    end
+    
+    for n in 1:N
+        ε[n] ~ Pareto(3, 2000) 
+        X[n, :] ~ MvNormal(μ[group[n], :], Σ) 
+    end 
+
+    ε = ε .- mean(ε)
+    for n in 1:N
+        y[n] = 12_000 + X[n, :] ⋅ β + u[group[n]] + ε[n]
+    end
+
+    return (y = y, X = X, ε = ε, u = u)
+end
+
+@model function gb2(N, D, K, group; ρ=0.2)
+    y, ε = initialize(2, N)
+    u = Vector{Float64}(undef, D)
+    X = Matrix{Float64}(undef, N, K)
+
+    μ = Matrix{Float64}(undef, D, K)
+    Σ = corr_matrix(K, ρ)    
+
+    β = repeat([-250], K)
+    
+    for d in 1:D
+        u[d] ~ Normal(0, 500)
+        for k in 1:K 
+            μ[d, k] ~ Uniform(-1, 1)
+        end
+    end
+    
+    for n in 1:N
+        ε[n] ~ GB2(2.5, 18, 1.46, 1700) 
+        X[n, :] ~ MvNormal(μ[group[n], :], Σ) 
+    end 
+
+    ε = ε .- mean(ε)
+    for n in 1:N
+        y[n] = 9_000 +  X[n, :] ⋅ β + u[group[n]] + ε[n]
+    end
+
+    return (y = y, X = X, ε = ε, μ = μ, u = u)
+end
+
+
+@model function logscale_simple(N, D, group)
     y, x, z, ε = initialize(4, N)
     μ, u = initialize(2, D)
     
@@ -23,7 +112,7 @@ initialize(n_var, dim) = [Vector{Float32}(undef, dim) for _ in 1:n_var]
     return (y = y, x = x, z = z, ε = ε, μ = μ, u = u)
 end
 
-@model function pareto(N, D, group)
+@model function pareto_simple(N, D, group)
     y, x, ε = initialize(4, N)
     μ, u = initialize(2, D)
     
@@ -45,7 +134,7 @@ end
     return (y = y, x = x, ε = ε, μ = μ, u = u)
 end
 
-@model function gb2(N, D, group)
+@model function gb2_simple(N, D, group)
     y, x, ε = initialize(3, N)
     μ, u = initialize(2, D)
     
@@ -66,3 +155,4 @@ end
 
     return (y = y, x = x, ε = ε, μ = μ, u = u)
 end
+
