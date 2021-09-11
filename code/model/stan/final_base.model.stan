8caaf599b5123b9d4022024e0f180844e880af83
flaviomorelli <38486294+flaviomorelli@users.noreply.github.com>
2021-09-11 10:50:29 +0200

    Finished first draft without introduction added difference between EBP and HB

---
+++ code/model/stan/final_base.model.stan
@@ -0,0 +1,82 @@
+functions{
+  real skewness(vector y){
+    int N = num_elements(y);
+    vector[N] yc = y - mean(y);
+    real s_3 = sd(y)^3;
+    real m_3 = mean(yc.^3);
+    return m_3 / s_3;
+  }
+}
+
+data {
+  int<lower=0> N;
+  int<lower=0> N_pred;
+  int<lower=0> K;
+  int<lower=0> D;
+  
+  vector<lower=0>[N] y;
+  matrix[N, K] X;
+  matrix[N_pred, K] X_pred;
+  matrix[D, D] W_tilde;
+  int domain[N];
+  int domain_pred[N_pred];
+}
+
+parameters {
+  real intercept;
+  real<lower=0> nu_raw;
+  real<lower=-min(y) + 0.1> lambda;
+  
+  real<lower=0> sigma;
+  real<lower=0> sigma_u;
+  vector[D] u_tilde;
+  
+  vector[K] beta;
+}
+transformed parameters{
+  vector[N] log_y = log(y + lambda);
+  real s = skewness(log_y);
+  real nu = nu_raw + 2;
+  real sigma_e = sigma * sqrt(nu - 2 / nu);
+  vector[D] u = sigma_u * u_tilde;
+}
+
+model {
+  intercept ~ normal(0, 5);
+  beta ~ normal(0, 0.2);
+  sigma ~ gamma(2, 7);
+  
+  sigma_u ~ gamma(2, 7);
+  u_tilde ~ std_normal();
+  // rho ~ normal(0, 0.4);
+  
+  // Shape parameters
+  nu_raw ~ gamma(2, 0.1);
+  s ~ normal(0, 0.01); 
+  
+  // Likelihood
+  vector[N] mu;
+  for(n in 1:N)
+    mu[n] = intercept + X[n] * beta + u[domain[n]];
+
+  log_y ~ student_t(nu, mu, sigma_e);
+  target += - log_y; // Jacobian adjustment
+}
+
+generated quantities{
+  vector[N] log_lik;
+  vector[N_pred] y_pred;
+  for (n in 1:N) {
+    log_lik[n] = student_t_lpdf(log_y[n] |nu,
+                        intercept + X[n] * beta + u[domain[n]],
+                        sigma_e)
+                        - log_y[n];
+  }
+  
+  
+  for(n in 1:N_pred){
+    y_pred[n] = exp(student_t_rng(nu, 
+                  intercept + X_pred[n] * beta + u[domain_pred[n]], sigma_e)) - lambda;
+  }
+  
+}
--- code/notebooks/final_model_indicators/indicators_result.R
+++ code/notebooks/final_model_indicators/indicators_result.R
@@ -2,11 +2,21 @@ library(tidyverse)
 library(cmdstanr)
 library(posterior)
 
+
 # Load data
 source(file.path("dataloader", "data_cleaning.R"))
 
 # Load numeric functions
 source(file.path("ops", "indicators.R"))
+
+# Load aggregated data
+mun_data <- readstata13::read.dta13(file.path("data", "mexico", 
+                                              "base_2donivel_Nacional_2010.dta")) %>%
+  as_tibble() %>% 
+  filter(nom_ent == "Guerrero") %>% 
+  select(mun, pobtot) %>% 
+  mutate(id = as.character(as.numeric(mun)), rel_pop = pobtot / sum(pobtot))
+
 
 # Set up map
 mexico <- raster::shapefile(file.path("data", "mexico_shapefile", "Muni_2012gw.shp"))
@@ -20,6 +30,9 @@ guerrero_df <- fortify(mexico[guerrero_obs, ], region = "CVE_MUN")
 sar_model <- cmdstan_model(file.path("model", "stan", 
                                      "final_sar.model.stan"))
 
+base_model <- cmdstan_model(file.path("model", "stan", 
+                                      "final_base.model.stan"))
+
 combined_list <- c(mcs_one_hot_strat, census_one_hot_strat)
 
 hb_fit <- sar_model$sample(combined_list, 
@@ -27,16 +40,29 @@ hb_fit <- sar_model$sample(combined_list,
                  iter_warmup = 1200, 
                  iter_sampling = 300)
 
-hb_fit$cmdstan_diagnose()
+base_fit <- base_model$sample(combined_list, 
+                           parallel_chains = 4, 
+                           iter_warmup = 1200, 
+                           iter_sampling = 300)
 
 
 hb_output <- read_cmdstan_csv(hb_fit$output_files(),
                               variables = "y_pred")
 
-hb_pred <- hb_output$post_warmup_draws %>% as_draws_matrix()
-write.csv(hb_pred, file.path("data", "predictions", "hb_pred.csv"))
+base_output <- read_cmdstan_csv(base_fit$output_files(),
+                              variables = "y_pred")
+
+
+write.csv(hb_output$post_warmup_draws %>% as_draws_matrix(), 
+          file.path("data", "predictions", "hb_pred.csv"))
+
+write.csv(base_output$post_warmup_draws %>% as_draws_matrix(), 
+          file.path("data", "predictions", "base_pred.csv"))
 # Load stored predictions
 hb_pred <- data.table::fread(file = file.path("data", "predictions", "hb_pred.csv")) %>% 
+  as.matrix %>% .[ , -1]
+
+base_pred <- data.table::fread(file = file.path("data", "predictions", "base_pred.csv")) %>% 
   as.matrix %>% .[ , -1]
 
 
@@ -46,7 +72,14 @@ hb_hcr <- fgt(t(hb_pred),
     type = "hcr", 
     w = census_one_hot_strat$factor)
 
+base_hcr <- fgt(t(base_pred), 
+              census_one_hot_strat$mun, 
+              max(census_one_hot_strat$mun), 
+              type = "hcr", 
+              w = census_one_hot_strat$factor)
+
 write.csv(hb_hcr, file.path("data", "predictions", "hb_hcr.csv"))
+write.csv(base_hcr, file.path("data", "predictions", "base_hcr.csv"))
 
 hb_pgap <- fgt(t(hb_pred), 
     census_one_hot_strat$mun, 
@@ -54,7 +87,14 @@ hb_pgap <- fgt(t(hb_pred),
     type = "pgap", 
     w = census_one_hot_strat$factor)
 
+base_pgap <- fgt(t(base_pred), 
+               census_one_hot_strat$mun, 
+               max(census_one_hot_strat$mun), 
+               type = "pgap", 
+               w = census_one_hot_strat$factor)
+
 write.csv(hb_pgap, file.path("data", "predictions", "hb_pgap.csv"))
+write.csv(base_pgap, file.path("data", "predictions", "base_pgap.csv"))
 
 ebp_indicators <- emdi::ebp(
   ictpc ~ pcpering + actcom_pc + bienes_pc +  jexp + jedad + pcocup + 
@@ -75,6 +115,8 @@ write.csv(sqrt(ebp_indicators$MSE[, 2:11]), file.path("data", "predictions", "eb
 # Loading stored indicators
 hb_hcr <- read_csv(file.path("data", "predictions", "hb_hcr.csv") ) %>% select(-X1)
 hb_pgap <- read_csv(file.path("data", "predictions", "hb_pgap.csv"))%>% select(-X1)
+base_hcr <- read_csv(file.path("data", "predictions", "base_hcr.csv") ) %>% select(-X1)
+base_pgap <- read_csv(file.path("data", "predictions", "base_pgap.csv"))%>% select(-X1)
 ebp_ind <- read_csv(file.path("data", "predictions", "ebp_ind.csv")) %>% 
   mutate(id = as.character(Domain)) %>% 
   select(id, Head_Count, Poverty_Gap) %>% 
@@ -85,20 +127,43 @@ ebp_rmse <- read_csv(file.path("data", "predictions", "ebp_rmse.csv")) %>%
   rename(ebp_hcr_rmse = Head_Count, ebp_pgap_rmse = Poverty_Gap)
 
 hcr <- data.frame(hb_hcr_mean = apply(hb_hcr, MARGIN = 1, mean),
-                  hb_hcr_sd = apply(hb_hcr, MARGIN = 1, sd)) %>% 
+                  hb_hcr_sd = apply(hb_hcr, MARGIN = 1, sd),
+                  base_hcr_mean = apply(base_hcr, MARGIN = 1, mean),
+                  base_hcr_sd = apply(base_hcr, MARGIN = 1, sd)) %>% 
   rowid_to_column(var = "id") %>% 
   mutate(id = as.character(id))
 
 pgap <- data.frame(hb_pgap_mean = apply(hb_pgap, MARGIN = 1, mean),
-                   hb_pgap_sd = apply(hb_pgap, MARGIN = 1, sd))%>% 
+                   hb_pgap_sd = apply(hb_pgap, MARGIN = 1, sd),
+                   base_pgap_mean = apply(base_pgap, MARGIN = 1, mean),
+                   base_pgap_sd = apply(base_pgap, MARGIN = 1, sd))%>% 
   rowid_to_column(var = "id") %>% 
   mutate(id = as.character(id))
+
+ind_diff <- data.frame(id = hcr$id, 
+                       HCR = (ebp_ind$ebp_hcr_mean - hcr$hb_hcr_mean),
+                       PGAP = (ebp_ind$ebp_pgap_mean - pgap$hb_pgap_mean))
+
+diff_plot <- ggplot(data = ind_diff %>% 
+         gather("Indicator", "val", -id), 
+       aes(x = as.numeric(id), y = val, color = Indicator)) + 
+  geom_line() + 
+  labs(x = "Municipality", y = "", color="") +
+  theme_minimal() + 
+  theme(text = element_text(size = 14))
+
+ggsave(file.path("notebooks", "final_model_indicators", "diff.png"), 
+       plot = diff_plot,
+       width = 16, 
+       height = 9, 
+       unit = "cm")
 
 guerrero_ind_df <- guerrero_df %>% 
   inner_join(hcr, by = "id") %>% 
   inner_join(pgap, by = "id") %>% 
   inner_join(ebp_ind, by = "id") %>% 
-  inner_join(ebp_rmse, by = "id")
+  inner_join(ebp_rmse, by = "id") %>% 
+  inner_join(ind_diff, by = "id")
 
 # Generate plots
 
@@ -112,19 +177,27 @@ guerrero_map <- function(data, variable, legend){
           legend.text = element_text(size = 14))
 }
 
-hb_hcr_mean_plot <- guerrero_map(guerrero_ind_df, "hb_hcr_mean", "HCR")
+hb_hcr_mean_plot <- guerrero_map(guerrero_ind_df, "hb_hcr_mean", "HCR") + 
+  scale_fill_gradient(low = "yellow", high = "red", limits = c(0, 0.78))
 hb_hcr_sd_plot <- guerrero_map(guerrero_ind_df, "hb_hcr_sd", "S.D. HCR")
-hb_pgap_mean_plot <- guerrero_map(guerrero_ind_df, "hb_pgap_mean", "PGAP")
+  
+hb_pgap_mean_plot <- guerrero_map(guerrero_ind_df, "hb_pgap_mean", "PGAP")+ 
+  scale_fill_gradient(low = "yellow", high = "red", limits = c(0, 0.5))
 hb_pgap_sd_plot <- guerrero_map(guerrero_ind_df, "hb_pgap_sd", "S.D. PGAP")
 hb_hcr_cv_plot <- guerrero_map(guerrero_ind_df, "hb_hcr_sd/hb_hcr_mean", "CV HCR")
 hb_pgap_cv_plot <- guerrero_map(guerrero_ind_df, "hb_pgap_sd/hb_pgap_mean", "CV PGAP")
 
-ebp_hcr_mean_plot <- guerrero_map(guerrero_ind_df, "ebp_hcr_mean", "HCR")
+ebp_hcr_mean_plot <- guerrero_map(guerrero_ind_df, "ebp_hcr_mean", "HCR")+ 
+  scale_fill_gradient(low = "yellow", high = "red", limits = c(0, 0.78))
 ebp_hcr_rmse_plot <- guerrero_map(guerrero_ind_df, "ebp_hcr_rmse", "RMSE HCR")
-ebp_pgap_mean_plot <- guerrero_map(guerrero_ind_df, "ebp_pgap_mean", "PGAP")
+ebp_pgap_mean_plot <- guerrero_map(guerrero_ind_df, "ebp_pgap_mean", "PGAP")+ 
+  scale_fill_gradient(low = "yellow", high = "red", limits = c(0, 0.5))
 ebp_pgap_rmse_plot <- guerrero_map(guerrero_ind_df, "ebp_pgap_rmse", "RMSE PGAP")
 ebp_hcr_cv_plot <- guerrero_map(guerrero_ind_df, "ebp_hcr_rmse/ebp_hcr_mean", "CV HCR")
 ebp_pgap_cv_plot <- guerrero_map(guerrero_ind_df, "ebp_pgap_rmse/ebp_pgap_mean", "CV PGAP")
+
+hcr_diff_plot <- guerrero_map(guerrero_ind_df, "hcr_diff", "HCR DIFF")
+pgap_diff_plot <- guerrero_map(guerrero_ind_df, "pgap_diff", "PGAP DIFF")
 
 save_plot <- purrr::partial(ggsave, width = 16, height = 9, unit = "cm")
 
@@ -143,3 +216,34 @@ save_plot(filename = str_c(graph_path, "/ebp_pgap_mean.png"), ebp_pgap_mean_plot
 save_plot(filename = str_c(graph_path, "/ebp_pgap_rmse.png"), ebp_pgap_rmse_plot)
 save_plot(filename = str_c(graph_path, "/ebp_hcr_cv.png"), ebp_hcr_cv_plot)
 save_plot(filename = str_c(graph_path, "/ebp_pgap_cv.png"), ebp_pgap_cv_plot)
+
+save_plot(filename = str_c(graph_path, "/hcr_diff.png"), hcr_diff_plot)
+save_plot(filename = str_c(graph_path, "/pgap_diff.png"), pgap_diff_plot)
+
+ggplot() +
+  geom_line(data = hcr, mapping = aes(x = as.numeric(id), y = hb_hcr_mean)) +
+  geom_line(data = ebp_ind, mapping = aes(x = as.numeric(id), y =ebp_hcr_mean), color = "blue")
+
+# Benchmarking
+emdi_direct <- emdi::direct("ictpc",
+             as.data.frame(mcs),
+             smp_domains = "mun",
+             )$ind %>% 
+  rename(id = Domain) 
+
+benchmarking_df <- emdi_direct %>% 
+  inner_join(hcr, by = "id") %>% 
+  inner_join(pgap, by = "id") %>% 
+  inner_join(ebp_ind, by = "id") %>% 
+  inner_join(mun_data, by = "id") %>% 
+  mutate(rel_pop_2 = pobtot / sum(pobtot))
+
+
+weighted.mean(benchmarking_df$Head_Count, w = benchmarking_df$rel_pop_2)
+weighted.mean(benchmarking_df$hb_hcr_mean, w = benchmarking_df$rel_pop_2)
+weighted.mean(benchmarking_df$ebp_hcr_mean, w = benchmarking_df$rel_pop_2)
+
+weighted.mean(benchmarking_df$Poverty_Gap, w = benchmarking_df$rel_pop_2)
+weighted.mean(benchmarking_df$hb_pgap_mean, w = benchmarking_df$rel_pop_2)
+weighted.mean(benchmarking_df$ebp_pgap_mean, w = benchmarking_df$rel_pop_2)
+
